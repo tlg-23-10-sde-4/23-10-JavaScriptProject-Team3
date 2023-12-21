960babcd63c3514cdd8c9ee50b6cc1f8289c10af
CJish <christopher.ulrich@tlgcohort.com>
2023-12-20 16:46:00 -0800

    Pretty sure I got the functionality working correctly; just needed to add another .then and move the chart creation in there.

--- sleep_page/script.js
+++ sleep_page/script.js
@@ -4,9 +4,11 @@ const sleepGoal = 7;
 const timeNow = new Date();
 const todayMonth = timeNow.getMonth()+1;
 const todayDate = timeNow.getDate();
-const arrHoursSleep = [5.5, 6.9, 6.13, 7.4, 7.3, 4.67, 5.67]
-const average = array => array.reduce((a, b) => a + b) / array.length;
-const avgHrsSleep = average(arrHoursSleep);
+const arrHoursSleep = []
+const sleepData = {}
+const avgHrsSleep = []
+const ctx = document.getElementById("sleepChart");
+let iterator = 0;
 
 // function that shows sleep time based on sleep schedule and phone usage
 
@@ -14,13 +16,34 @@ const avgHrsSleep = average(arrHoursSleep);
 
 // Displaying the average hours of sleep over the past week
 const pointerAvgSleep = document.getElementById("averageSleep")
-pointerAvgSleep.textContent = `You averaged ${avgHrsSleep.toFixed(2)} hours of sleep over the past week`
+// pointerAvgSleep.textContent = `You averaged ${avgHrsSleep.toFixed(2)} hours of sleep over the past week`
 
 //3-dot functionality usage order of operations
 
+// Fetch function
+function getFitnessData() {
+  fetch("https://api.fitbit.com/1.2/user/-/sleep/date/2023-12-14/2023-12-20.json", {
+    headers: {
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1JRVDYiLCJzdWIiOiJCVjRNSlIiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd2VjZyB3c29jIHdhY3Qgd294eSB3dGVtIHd3ZWkgd2NmIHdzZXQgd3JlcyB3bG9jIiwiZXhwIjoxNzAzMTM3NzQwLCJpYXQiOjE3MDMxMDg5NDB9.-iR97wccsOb_MTMT3disKdsqYFCFp58cjt0SvrZNZOc"
+    }
+  })
+  .then((res) => res.json()) 
+  .then((sleepData) => {
+    // console.log(sleepData);
+    for (let elt of sleepData.sleep) {
+      arrHoursSleep.push(`${(sleepData.sleep[iterator].minutesAsleep)/60}`)
+      console.log((sleepData.sleep[iterator].minutesAsleep) / 60)
+      iterator++
+    }
+    // console.log(arrHoursSleep);
+  })
+  .catch()
+}
+
+getFitnessData();
 
 // graph of sleep time over past 7 days
-const ctx = document.getElementById("sleepChart");
+
 
 new Chart(ctx, {
   type: "bar",
@@ -52,6 +75,8 @@ new Chart(ctx, {
     ],
   },
   options: {
+    responsive: true,
+    maintainAspectRatio: false,
     scales: {
       y: {
         beginAtZero: true,
@@ -61,3 +86,9 @@ new Chart(ctx, {
 });
 
 //function to enter sleep data manually
+
+// function to calculate average hours of sleep over the past week
+function calculateAvgSleep(array) {
+  const average = array => array.reduce((a, b) => a + b) / array.length;
+  const avgHrsSleep = average(arrHoursSleep);
+}
